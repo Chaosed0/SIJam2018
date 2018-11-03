f9a3490f565b6ab111e37124bcb5bb7cea04b17a
chaosed0 <chaosed0@gmail.com>
2018-11-03 14:12:19 -0700

    A tiny amount of gun follow

--- Assets/SpaceBlast/Scripts/ViewModelFollow.cs
+++ Assets/SpaceBlast/Scripts/ViewModelFollow.cs
@@ -3,16 +3,29 @@ using System.Collections;
 
 public class ViewModelFollow : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject follow;
 
-    // Use this for initialization
-    void Start()
+    [SerializeField]
+    private float rotationLag = 0.2f;
+
+    float rotationVelocity = 0f;
+    Quaternion rotationOffset = Quaternion.identity;
+
+    private void Awake()
     {
-
+        rotationOffset = transform.localRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        transform.parent = null;
+    }
 
+    void FixedUpdate()
+    {
+        transform.position = follow.transform.position;
+        float newAngle = Mathf.SmoothDampAngle(Quaternion.Angle(transform.rotation, follow.transform.rotation), 0f, ref rotationVelocity, rotationLag);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, follow.transform.rotation, newAngle);
     }
 }
