d7c8b1abc6da783b53734f090de86b0d4fa22635
chaosed0 <chaosed0@gmail.com>
2018-11-03 11:49:45 -0700

    Pixelation and hallways

---
+++ Assets/Pixelation/Scripts/Pixelation.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Pixelation.Example.Scripts;
+using UnityEngine;
+
+namespace Assets.Pixelation.Scripts
+{
+    [ExecuteInEditMode]
+    [AddComponentMenu("Image Effects/Color Adjustments/Pixelation")]
+    public class Pixelation : ImageEffectBase
+    {
+        public float BlockCount = 128;
+        public float ColorStep = 64;
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            float k = Camera.main.aspect;
+            Vector2 count = new Vector2(BlockCount, BlockCount/k);
+            material.SetVector("BlockCount", count);
+            material.SetFloat("ColorStep", ColorStep);
+            Graphics.Blit(source, destination, material);
+        }
+    }
+}
---
+++ Assets/Pixelation/Shaders/Pixelation.shader
@@ -0,0 +1,30 @@
+﻿Shader "Hidden/Pixelation"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;	
+			float2 BlockCount;
+			float ColorStep;
+
+			fixed4 frag (v2f_img i) : SV_Target
+			{
+				float4 tex = tex2D(_MainTex, floor(i.uv * BlockCount) / BlockCount + 1 / (BlockCount * 2));
+				tex.rgb = round(tex.rgb * ColorStep) / ColorStep;
+				return tex;
+			}
+			ENDCG
+		}
+	}
+}
