3f11e926f66ee7849316c231aeebe6c6ec8f7e57
chaosed0 <chaosed0@gmail.com>
2018-11-04 15:51:23 -0800

    Astronaut thing

---
+++ Assets/SpaceBlast/Scripts/PlayIfNotDead.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayIfNotDead : MonoBehaviour
+{
+    [SerializeField]
+    private Health health;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private bool played = false;
+
+    public void Play()
+    {
+        if (played)
+        {
+            return;
+        }
+
+        played = true;
+        if (health.GetHealth() > 0f)
+        {
+            audioSource.Play();
+        }
+    }
+}
