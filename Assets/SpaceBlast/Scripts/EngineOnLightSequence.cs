3080775601ae6f1c6771fe69c6fc34101dabf93d
NickNamelous <guerillarenegade@yahoo.com>
2018-11-12 10:35:30 -0800

    Lights edit.
    Extra lighting. Engine lights animation. Vent frames, circuitbox added.

---
+++ Assets/SpaceBlast/Scripts/EngineOnLightSequence.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineOnLightSequence : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	private void Awake () {
+		anim = GetComponent<Animator> ();
+		anim.SetBool ("EngineOn", true);
+	}
+}
